9183e5f40dcfd040c05d39c6d3b9643ff792d547
Donny Techiera <DonnyTech20@gmail.com>
2019-04-20 15:06:00 -0400

    added axios api

--- index.android.js
+++ index.android.js
@@ -6,10 +6,11 @@ import AlbumList from './src/components/AlbumList';
 // Create a component 
 const App = () => {
   <View>
-  <Header headerPassDownProperty={'Don\'s Albums'}  />
+  <Header headerPassDownProperty={'Albums'}  />
   <AlbumList />
 </View>
 
 };
 
-  export default App;
+  // Render it to device
+  AppRegistry.registerComponent('firstnativeproject', () => App);
