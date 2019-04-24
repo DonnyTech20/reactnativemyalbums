fbaac0f8028ab685e04deb802e20155fa925babc
Donny Techiera <DonnyTech20@gmail.com>
2019-04-24 15:32:29 -0400

    added buttonstyle

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
 const Buttons = () => {
+    const { buttonStyle } = styles;
+    const { textStyle } = styles;
+
     return (
-        <Text>
+        <TouchableOpacity style={buttonStyle}>
+        <Text style={textStyle}>
             Press me!!
         </Text>
+        </TouchableOpacity>
     );
+};
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroudColor: '#fff',
+        borderColor: '#007aff',
+        marginLeft: 5,
+        maginRight: 5
+    }
 };
 
 export default Buttons;
